264d2bed89f7bceaacb64b3c37a30e8d5be4fdd8
pjtinker <pjtinker42@students.tntech.edu>
2019-09-15 13:17:31 -0500

    Changed tokenizer transformermixin

--- package/utils/TokenizerTransformer.py
+++ package/utils/TokenizerTransformer.py
@@ -4,18 +4,19 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class TokenizerTransformer(BaseEstimator, TransformerMixin, Tokenizer):
 
-    def __init__(self, **tokenizer_params):
-        Tokenizer.__init__(self, **tokenizer_params)
-
+    def __init__(self, tokenizer, **tokenizer_params):
+        # Tokenizer.__init__(self, **tokenizer_params)
+        self.tokenizer = tokenizer
+        
     def fit(self, X, y=None):
-        self.fit_on_texts(X)
+        # self.tokenizer.fit_on_texts(X)
         return self
 
     def transform(self, X, y=None):
-        X_transformed = self.texts_to_sequences(X)
+        X_transformed = self.tokenizer.texts_to_sequences(X)
         return X_transformed
     
     def get_num_features(self):
         print("get_num_features called from TokenizerTransformer")
-        print(f"num_features: {len(self.word_index)}")
-        return min(len(self.word_index) + 1, 20000)
+        print(f"num_features: {len(self.tokenizer.word_index)}")
+        return min(len(self.tokenizer.word_index) + 1, 20000)
